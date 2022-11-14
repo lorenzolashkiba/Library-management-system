20a77570c61d701b5c0652a359ef8a3410072f19
lorenzolashkiba <lorenzoboiko@gmail.com>
2022-11-14 22:48:48 +0100

    setup

--- src/db_connect.php
+++ src/db_connect.php
@@ -1,5 +1,5 @@
 <?php
-	$con = mysqli_connect('localhost', 'root', '', 'library_db');
+	$con = mysqli_connect('localhost', 'root', 'root', 'library');
 	if(!$con)
 		die("ERROR: Couldn't connect to database");
 ?>
